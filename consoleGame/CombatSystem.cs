cd9750dc1d7fba72deb48c6e5a9bdb12cec609b3
Jan Horák <jan.342003@seznam.cz>
2021-01-07 17:13:40 +0100

    first commit

---
+++ consoleGame/CombatSystem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Console = Colorful.Console;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace consoleGame
+{
+    class CombatSystem
+    {
+        //array of sentences - update to api in the future
+        string[] targetText = new string[]
+        { "Hello my name is John."
+        , "Our school has awesome teachers."
+        , "Truth in advertising and dinosaurs with skateboards have much in common."
+        , "Europe is almost like a cheese."
+        , "Math is a rewarding subject."
+        , "Sometimes, all you need to do is completely make an ass of yourself and laugh it off to realise that life isn't so bad after all."
+        , "He barked orders at his daughters but they just stared back with amusement."
+        , "The gloves protect my feet from excess work."};
+
+        //variable with instance of random method
+        Random random = new Random();
+     
+        public bool Attack()
+        {
+            //variable with random array index number
+            int num = random.Next(targetText.Length);
+
+            //show the random array index number
+            Console.Write("{0}.) ", num + 1);
+            //shows the value of random array index number
+            Console.Write("{0}\n", targetText[num]);
+
+            //stopwatch expcet it locks me for the duration of the timer
+            
+            //var time = new Stopwatch();
+            //time.Measure();
+
+            //user input
+            Console.Write("here: ");
+            string input = Console.ReadLine();
+
+            // if/else comparison of user input to the sentence from array
+            if (input == targetText[num])
+            {
+                Console.WriteLine("\nAttack was successful.\n");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("\nAttack failed.\n");
+                return false;
+            }
+        }
+    }
+}
---
+++ consoleGame/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+using System.Drawing;
+
+namespace consoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = Color.Black;
+            Console.BackgroundColor = Color.AliceBlue;
+
+            Console.Clear();
+
+            Console.WriteLine(@"
+
+
+
+██████╗ ██╗   ██╗███╗   ██╗ ██████╗ ███████╗ ██████╗ ███╗   ██╗        ██████╗ ███████╗       ██████╗  ██████╗  ██████╗ ███╗   ███╗
+██╔══██╗██║   ██║████╗  ██║██╔════╝ ██╔════╝██╔═══██╗████╗  ██║       ██╔═══██╗██╔════╝       ██╔══██╗██╔═══██╗██╔═══██╗████╗ ████║
+██║  ██║██║   ██║██╔██╗ ██║██║  ███╗█████╗  ██║   ██║██╔██╗ ██║       ██║   ██║█████╗         ██║  ██║██║   ██║██║   ██║██╔████╔██║
+██║  ██║██║   ██║██║╚██╗██║██║   ██║██╔══╝  ██║   ██║██║╚██╗██║       ██║   ██║██╔══╝         ██║  ██║██║   ██║██║   ██║██║╚██╔╝██║
+██████╔╝╚██████╔╝██║ ╚████║╚██████╔╝███████╗╚██████╔╝██║ ╚████║       ╚██████╔╝██║            ██████╔╝╚██████╔╝╚██████╔╝██║ ╚═╝ ██║
+╚═════╝  ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝        ╚═════╝ ╚═╝            ╚═════╝  ╚═════╝  ╚═════╝ ╚═╝     ╚═╝
+                                                                                                                                   
+
+
+
+                                                                          ", Color.DarkRed);
+            System.Console.WriteLine("Press any key to start the game");
+            Console.ReadKey(true);
+
+            var player = new Character("Dravolas", "Male", 50, 200);
+            player.Stats();
+
+            var computer = new Character("Computer", "unknown", 50, 200);
+            computer.Stats();
+
+            var challenge = new CombatSystem();
+
+            do
+            {
+                if (challenge.Attack() == true)
+                {
+                    player.Attack(computer);
+                    computer.Stats();
+                }
+                else
+                {
+                    computer.Attack(player);
+                    player.Stats();
+                }
+            } while (player.IsAlive == true && computer.IsAlive == true);
+
+            if(player.IsAlive == true)
+            {
+                Console.WriteLine("\nCongratulations, you won the game !");
+            }else if (player.IsAlive == false)
+            {
+                Console.WriteLine("\nYou lost.");
+            }
+            else
+            {
+                Console.WriteLine("Unknown error has occured.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
